94672b36acf930faef4ca2a00a61d71bdd09938d
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2012-12-19 10:23:48 +0100

    feat(app): expose use-case factories via index

---
+++ test/application/index.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var expect = require('chai').expect;
+var application = require('../../lib/src/application');
+
+describe('application index', function () {
+  it('re-exports the use-case factories', function () {
+    expect(application.indexNews.create).to.be.a('function');
+    expect(application.queryByRegion.create).to.be.a('function');
+  });
+});
