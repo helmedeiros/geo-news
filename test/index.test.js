3ab975cb2954bb4c0a372de8d1ce7f6d03c761e7
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2012-12-27 14:55:33 +0100

    feat: wire composition root with default adapters

--- test/index.test.js
+++ test/index.test.js
@@ -8,4 +8,16 @@ describe('geo-news', function () {
     expect(geoNews.version).to.be.a('string');
     expect(geoNews.version).to.match(/^\d+\.\d+\.\d+$/);
   });
+
+  it('re-exports domain, application and default adapters', function () {
+    expect(geoNews.domain.newsItem.create).to.be.a('function');
+    expect(geoNews.application.queryByRegion.create).to.be.a('function');
+    expect(geoNews.adapters.inMemoryRepository.create).to.be.a('function');
+  });
+
+  it('wires a composition root with sensible defaults', function () {
+    var wired = geoNews.wire({});
+    expect(wired.indexNews.execute).to.be.a('function');
+    expect(wired.queryByRegion.execute).to.be.a('function');
+  });
 });
