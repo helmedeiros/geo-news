835428720e1ab2f9bae63b33a1392ef6195a9026
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2013-01-08 11:42:18 +0100

    feat(adapters): add RssAdapter skeleton over injected http and parser

---
+++ test/adapters/rss-adapter.test.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var expect = require('chai').expect;
+var rssAdapter = require('../../lib/src/adapters/rss-adapter');
+var portal = require('../../lib/src/domain/portal');
+var newsFeedPort = require('../../lib/src/ports/news-feed-port');
+
+var clarin = portal.create({
+  id: 'ar-clarin', name: 'Clarin', country: 'AR',
+  city: 'Buenos Aires', lat: -34.61, lon: -58.38, rss: 'http://example.com/rss'
+});
+
+function fakeHttp(body, err) {
+  return {
+    get: function (url, callback) {
+      process.nextTick(function () { callback(err || null, err ? null : body); });
+    }
+  };
+}
+
+function fakeParser(entries) {
+  return { parse: function () { return entries; } };
+}
+
+describe('RssAdapter', function () {
+  it('satisfies the NewsFeedPort contract', function () {
+    var adapter = rssAdapter.create({
+      httpClient: fakeHttp(''),
+      parser: fakeParser([])
+    });
+    expect(newsFeedPort.isImplementation(adapter)).to.equal(true);
+  });
+
+  it('maps parsed entries to NewsItem-shaped objects with the portal id', function (done) {
+    var entries = [
+      {
+        title: 'Hello', link: 'http://x/1', summary: 'lorem',
+        publishedAt: new Date('2013-01-08T00:00:00Z')
+      }
+    ];
+    var adapter = rssAdapter.create({
+      httpClient: fakeHttp('<rss/>'),
+      parser: fakeParser(entries)
+    });
+    adapter.fetch(clarin, function (err, items) {
+      expect(err).to.equal(null);
+      expect(items).to.have.length(1);
+      expect(items[0].portalId).to.equal('ar-clarin');
+      expect(items[0].id).to.equal('ar-clarin:http://x/1');
+      done();
+    });
+  });
+
+  it('propagates HTTP errors to the callback', function (done) {
+    var adapter = rssAdapter.create({
+      httpClient: fakeHttp(null, new Error('network')),
+      parser: fakeParser([])
+    });
+    adapter.fetch(clarin, function (err) {
+      expect(err.message).to.equal('network');
+      done();
+    });
+  });
+});
