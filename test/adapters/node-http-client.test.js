c630740987c09a378f063e96110ac9241e446fdc
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2013-02-19 16:08:33 +0100

    feat(adapters): add NodeHttpClient backing the RssAdapter

---
+++ test/adapters/node-http-client.test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var expect = require('chai').expect;
+var http = require('http');
+var nodeHttpClient = require('../../lib/src/adapters/node-http-client');
+
+describe('NodeHttpClient', function () {
+  var server;
+  var port;
+
+  before(function (done) {
+    server = http.createServer(function (req, res) {
+      if (req.url === '/ok') {
+        res.writeHead(200, { 'content-type': 'application/xml' });
+        return res.end('<rss>hi</rss>');
+      }
+      res.writeHead(404);
+      res.end('nope');
+    });
+    server.listen(0, function () {
+      port = server.address().port;
+      done();
+    });
+  });
+
+  after(function (done) { server.close(done); });
+
+  it('returns the body for a 2xx response', function (done) {
+    var client = nodeHttpClient.create();
+    client.get('http://127.0.0.1:' + port + '/ok', function (err, body) {
+      expect(err).to.equal(null);
+      expect(body).to.equal('<rss>hi</rss>');
+      done();
+    });
+  });
+
+  it('reports a non-2xx response as an error', function (done) {
+    var client = nodeHttpClient.create();
+    client.get('http://127.0.0.1:' + port + '/missing', function (err) {
+      expect(err.message).to.match(/HTTP 404/);
+      done();
+    });
+  });
+});
