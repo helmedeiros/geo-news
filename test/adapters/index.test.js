4809ec1f7d065606e2c4305a42030a3c14adbff9
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2013-03-19 15:33:48 +0100

    feat(adapters): expose every adapter via a single index

---
+++ test/adapters/index.test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var expect = require('chai').expect;
+var adapters = require('../../lib/src/adapters');
+
+describe('adapters index', function () {
+  it('exposes every adapter and helper', function () {
+    expect(adapters.inMemoryRepository.create).to.be.a('function');
+    expect(adapters.inMemoryPortalRegistry.create).to.be.a('function');
+    expect(adapters.systemClock.now).to.be.a('function');
+    expect(adapters.rssAdapter.create).to.be.a('function');
+    expect(adapters.rssParser.parse).to.be.a('function');
+    expect(adapters.nodeHttpClient.create).to.be.a('function');
+    expect(adapters.htmlEntities.decode).to.be.a('function');
+    expect(adapters.feedErrors.http).to.be.a('function');
+  });
+});
