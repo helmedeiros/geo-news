b843bb9e9e839ad74a24c77b65c7fca2112399e7
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2013-08-29 11:33:27 +0200

    feat: wire IndexAllUseCase into the composition root

--- test/application/index.test.js
+++ test/application/index.test.js
@@ -6,6 +6,7 @@ var application = require('../../lib/src/application');
 describe('application index', function () {
   it('re-exports the use-case factories', function () {
     expect(application.indexNews.create).to.be.a('function');
+    expect(application.indexAll.create).to.be.a('function');
     expect(application.queryByRegion.create).to.be.a('function');
   });
 });
