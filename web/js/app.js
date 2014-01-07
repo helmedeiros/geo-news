501a2b2b21ff4a2f731726ed83d3fdeabde366e8
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2014-01-07 10:42:18 +0100

    feat(web): scaffold static UI with header, map slot and sidebar

---
+++ web/js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Bootstrap stub. The map, mode toggle and headline list come online in
+ * subsequent commits; for now this file simply marks the document body when
+ * everything has loaded so the deploy can be smoke-tested.
+ */
+
+(function () {
+  document.body.setAttribute('data-app', 'geo-news');
+}());
