b417be010a12ff92659bc7a4598e5533c7d6e2bf
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2014-02-05 11:18:27 +0100

    feat(web): add headline list view backed by a Backbone collection

---
+++ web/js/views/headline-list-view.js
@@ -0,0 +1,41 @@
+/* global $, Backbone, _ */
+'use strict';
+
+(function () {
+  var TEMPLATE = _.template(
+    '<div class="headline">' +
+      '<a href="<%= link %>" target="_blank"><%- title %></a>' +
+      '<div class="meta">' +
+        '<%- portalId %> · <%- publishedAt %>' +
+      '</div>' +
+    '</div>'
+  );
+
+  window.GeoNewsHeadlineListView = Backbone.View.extend({
+    el: '#headlines',
+
+    initialize: function (options) {
+      this.headlines = options.headlines;
+      this.listenTo(this.headlines, 'reset change', this.render);
+      this.render();
+    },
+
+    render: function () {
+      var items = this.headlines.toJSON();
+      if (items.length === 0) {
+        this.$el.html('<p class="empty">No headlines in this view yet.</p>');
+        return this;
+      }
+      var html = items.map(function (i) {
+        return TEMPLATE({
+          link: i.link,
+          title: i.title,
+          portalId: i.portalId,
+          publishedAt: new Date(i.publishedAt).toLocaleString()
+        });
+      }).join('');
+      this.$el.html(html);
+      return this;
+    }
+  });
+}());
