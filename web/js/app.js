6f9781cadc59903157f2ab94534ce953f7bab4fa
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2014-03-06 14:55:33 +0100

    feat(web): load the bundled dataset on boot and drive the controller

--- web/js/app.js
+++ web/js/app.js
@@ -1,4 +1,5 @@
-/* global Backbone, GeoNewsMapView, GeoNewsModeToggleView */
+/* global $, Backbone, GeoNewsMapView, GeoNewsModeToggleView,
+          GeoNewsHeadlineListView, GeoNewsHeadlines, GeoNewsAppController */
 'use strict';
 
 (function () {
@@ -7,14 +8,30 @@
   var RegionQuery = Backbone.Model.extend({
     defaults: {
       mode: 'publisher',
-      south: -60,
-      west: -90,
-      north: 15,
-      east: -30
+      south: -60, west: -90, north: 15, east: -30
     }
   });
 
   var regionQuery = new RegionQuery();
-  new GeoNewsMapView({ regionQuery: regionQuery });
+  var headlines = new GeoNewsHeadlines();
+  var mapView = new GeoNewsMapView({ regionQuery: regionQuery });
   new GeoNewsModeToggleView({ regionQuery: regionQuery });
+  new GeoNewsHeadlineListView({ headlines: headlines });
+
+  $.when(
+    $.getJSON('data/headlines.sample.json'),
+    $.getJSON('data/portals.sample.json')
+  ).done(function (headlinesResult, portalsResult) {
+    new GeoNewsAppController({
+      regionQuery: regionQuery,
+      headlines: headlines,
+      dataset: headlinesResult[0],
+      registry: portalsResult[0],
+      mapView: mapView
+    });
+  }).fail(function () {
+    $('#headlines').html(
+      '<p class="empty">Could not load the bundled dataset.</p>'
+    );
+  });
 }());
