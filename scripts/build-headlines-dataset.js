bb22e4412ff65f185cb38dc7dd248cc8cd43195a
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2014-07-29 14:42:09 +0200

    feat(scripts): enrich dataset with OG image and description

--- scripts/build-headlines-dataset.js
+++ scripts/build-headlines-dataset.js
@@ -5,8 +5,10 @@
  * scripts/build-headlines-dataset.js
  *
  * Fetches the bundled portals once, runs the gazetteer extractor over the
- * returned items and writes `web/data/headlines.json`. The UI loads this file
- * first and falls back to `headlines.sample.json` when the build has not run.
+ * returned items and writes `web/data/headlines.json`. After indexing, the
+ * top OG_LIMIT newest items are enriched with OpenGraph metadata (image,
+ * description) so the UI can render a click-to-preview panel without ever
+ * re-fetching the publisher's article body.
  *
  * Intended to be invoked during a Pages deploy or a periodic cron.
  */
@@ -18,9 +20,12 @@ var rssAdapter = require('../lib/src/adapters/rss-adapter');
 var rssParser = require('../lib/src/adapters/rss-parser');
 var nodeHttpClient = require('../lib/src/adapters/node-http-client');
 var gazetteerExtractor = require('../lib/src/adapters/gazetteer-extractor');
+var ogExtractor = require('../lib/src/adapters/og-extractor');
 var gazetteer = require('../lib/src/registry/gazetteer');
 
 var TARGET = path.join(__dirname, '..', 'web', 'data', 'headlines.json');
+var OG_LIMIT = 400;
+var OG_CONCURRENCY = 8;
 
 var wired = geoNews.wire({
   feed: rssAdapter.create({
@@ -30,6 +35,48 @@ var wired = geoNews.wire({
   extractor: gazetteerExtractor.create({ gazetteer: gazetteer }),
   concurrency: 4
 });
+
+var og = ogExtractor.create({
+  httpClient: nodeHttpClient.create({ timeoutMs: 6000, maxRedirects: 4 })
+});
+
+function enrichAll(items, done) {
+  var sorted = items.slice().sort(function (a, b) {
+    return new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime();
+  });
+  var queue = sorted.slice(0, OG_LIMIT);
+  var inFlight = 0;
+  var idx = 0;
+  var ok = 0;
+  var failed = 0;
+
+  function pump() {
+    if (idx >= queue.length && inFlight === 0) {
+      return done({ ok: ok, failed: failed, attempted: queue.length });
+    }
+    while (inFlight < OG_CONCURRENCY && idx < queue.length) {
+      runOne(queue[idx]);
+      idx = idx + 1;
+    }
+  }
+
+  function runOne(item) {
+    inFlight = inFlight + 1;
+    og.extract(item.link, function (err, meta) {
+      inFlight = inFlight - 1;
+      if (!err && meta) {
+        if (meta.image)       { item.image = meta.image; }
+        if (meta.description) { item.preview = meta.description; }
+        ok = ok + 1;
+      } else {
+        failed = failed + 1;
+      }
+      pump();
+    });
+  }
+
+  pump();
+}
 
 wired.indexAll.execute(function (err, summary) {
   if (err) {
@@ -41,8 +88,14 @@ wired.indexAll.execute(function (err, summary) {
       console.error('findAll failed:', findErr);
       process.exit(1);
     }
-    fs.writeFileSync(TARGET, JSON.stringify(items, null, 2));
-    console.log('wrote', items.length, 'items;',
-      summary.failures.length, 'failures');
+    console.log('indexed', items.length, 'items;',
+      summary.failures.length, 'portal failures');
+    console.log('fetching OG metadata for the newest', OG_LIMIT, 'items…');
+    enrichAll(items, function (ogSummary) {
+      console.log('OG enriched:', ogSummary.ok, 'ok,',
+        ogSummary.failed, 'failed of', ogSummary.attempted);
+      fs.writeFileSync(TARGET, JSON.stringify(items, null, 2));
+      console.log('wrote', items.length, 'items to', TARGET);
+    });
   });
 });
