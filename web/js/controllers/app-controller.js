cf67b73db753a60426f66f168fa20d47889a4176
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2014-07-30 09:42:18 +0200

    feat(web): sort sidebar headlines newest first

--- web/js/controllers/app-controller.js
+++ web/js/controllers/app-controller.js
@@ -17,6 +17,11 @@
       var matches = this.headlines.queryFor(
         this.dataset, this.regionQuery.attributes, this.registry
       );
+      matches.sort(function (a, b) {
+        return new Date(b.publishedAt).getTime() -
+               new Date(a.publishedAt).getTime();
+      });
+      this.headlines.reset(matches);
       this.mapView.renderItems(matches);
     }
   });
